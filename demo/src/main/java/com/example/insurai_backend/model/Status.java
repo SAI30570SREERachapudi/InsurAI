6867e31c2130b49895bba2f958ba9120c5cd2e12
SAI30570SREERachapudi <saisreegithub@gmail.com>
2025-12-02 14:21:20 +0530

    Milestone4 completed

--- demo/src/main/java/com/example/insurai_backend/model/Status.java
+++
@@ -1,9 +0,0 @@
-package com.example.insurai_backend.model;
-
-
-public enum Status {
-    PENDING,
-    APPROVED,
-    REJECTED
-}
-
--- demo/src/main/java/com/example/insurai_backend/repository/PolicyRepository.java
+++
@@ -1,8 +0,0 @@
-
-package com.example.insurai_backend.repository;
-import com.example.insurai_backend.model.Policy;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-public interface PolicyRepository extends JpaRepository<Policy, Long> {
-}
